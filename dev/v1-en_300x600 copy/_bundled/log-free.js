bed5a8df6693f19dcea5da44344620aa30b29419
Gar <gar@lonelydatu.com>
2020-10-30 13:09:43 -0400

    .

--- dev/v1-en_300x600 copy/_bundled/log-free.js
+++ dev/v1-en_300x600 copy/_bundled/log-free.js
@@ -8,22 +8,33 @@ function start() {
 	tl.set(".frame1", { opacity: 1 });
 
 	tl.from(".t1", .5, { x: "-=300", opacity: 0 }, "+=.2");
-	tl.to(".t1", .5, { x: "+=300", opacity: 0 }, "+=3.5");
+	tl.to(".t1", .5, { x: "+=300", opacity: 0 }, "+=3");
 	tl.from(".t2", .5, { x: "-=300", opacity: 0 });
+
+	tl.add("tape");
+	tl.add(tape(), "tape");
 
 	// tl.set(".frame2", {className: "+=in"}, "+=.6")
 
-	tl.add("end", "+=4");
-	tl.to(".t2", .5, { x: "+=300", opacity: 0 }, "end");
-	tl.to(".logo", .5, { opacity: 0 }, "end");
-	tl.to(".hero", .5, { opacity: 0 }, "end");
-
-	tl.set(".frame2", { opacity: 1 });
-	// tl.from(".bg", .4, {y:"-=600", opacity:0, ease:Power1.easeIn})
-	tl.from(".logo2", .4, { opacity: 0, ease: Power3.easeIn }, "+=.1");
-	tl.from(".t3", .4, { opacity: 0, ease: Power3.easeIn }, "+=.3");
+	tl.set(".frame2", { opacity: 1 }, "+=3");
+	tl.from(".bg_2", .4, { y: "-=600", opacity: 0, ease: Power1.easeIn });
+	tl.from(".logo", .4, { opacity: 0, ease: Power1.easeIn }, "-=.2");
+	tl.from(".tagline", .5, { clip: "rect(0px 0px 1200px 0px)" }, "+=.2");
 
 	// tl.gotoAndPlay("tape")
+}
+
+function tape() {
+	var TIME = .8;
+	var tl = new TimelineMax();
+
+	tl.to(".tape_2", TIME, { clip: "rect(0px 600px 1200px 600px)" }, .1);
+
+	tl.to(".tape_6", TIME, { clip: "rect(0px 600px 1200px 600px)" }, .3);
+	tl.to(".tape_4", TIME, { clip: "rect(0px 1200px 0px)" }, .2);
+	tl.to(".tape_5", TIME, { clip: "rect(0px 600px 1200px 600px)" }, .5);
+
+	return tl;
 }
 
 start();
